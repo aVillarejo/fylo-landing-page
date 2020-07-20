af341864e98f9ffb30ebe81e1be73d92d2674876
Alberto Villarejo <a.villarejo19@icloud.com>
2020-07-20 03:11:15 -0500

    setup

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-import styled from 'styled-components'
+import React, { useEffect } from 'react';
+import Router from 'next/router';
+import styled from 'styled-components';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
-export default function Home() {
-  return <Title>My page</Title>
+export default function Index() {
+  useEffect(() => {
+    const { pathname } = Router;
+    if (pathname == '/') {
+      Router.push('/home');
+    }
+  });
+  return <Title>Next with styled-components</Title>;
 }
